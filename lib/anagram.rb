7b4a8c4fbfa210d5bdc082e721cd32321fbad54c
bruce willis <fashun_vyctum@yahoo.com>
2020-04-14 23:22:46 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+def Anagram
+  
+  attr_accessor :anagram 
+  
+  def initialize(anagram)
+    @anagram = anagram
+  end
+  
+  def match(word_array)
+    word_array.select do |word|
+      word.split("").sort == @word.split("").sort
+    end
+  end
+end
+
+listen = Anagram.new("listen")
+listen.match(%w(enlists google inlets inlest banana))
