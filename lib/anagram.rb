0bed1856775beeb841e8a7d6cd781f86f41d03e0
bruce willis <fashun_vyctum@yahoo.com>
2020-04-14 23:29:41 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,14 @@
-def Anagram
-  
+class Anagram
+
   attr_accessor :word
-  
+
   def initialize(word)
     @word = word
   end
-  
-  def match(array_of_words)
-    array_of_words.select do |element|
-      (@word.split("").sort) == (element.split("").sort)
+
+  def match(word_array)
+    word_array.select do |word|
+      word.split("").sort == @word.split("").sort
     end
   end
 end
-
